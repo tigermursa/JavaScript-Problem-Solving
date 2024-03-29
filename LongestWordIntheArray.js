dab28a66e6fcae9b11bc3fac41ec79605482dd48
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-29 23:27:17 +0600

    "problem number one finding the longest word solved"

--- LongestWordIntheArray.js
+++ LongestWordIntheArray.js
@@ -1,1 +1,42 @@
-// Problem : Find
+//=======================================================
+//     Problem : Find the Longest word in a String
+//=======================================================
+
+//? Note:
+// If input string empty or only space then return invalid input
+
+// Solve-1: solving without using reduce
+const text = "feel the  code from your  heart of javaScript";
+
+const longestString = (string) => {
+  if (string.trim().length === 0) {
+    return "invalid input";
+  }
+
+  word = string.split(" ").sort((a, b) => a.length - b.length);
+  //   return word[word.length - 1]; // old way
+  return word.at(-1);
+};
+
+const result = longestString(text);
+
+console.log(result);
+
+// Solve-2: solving  using reduce
+
+const longestString2 = (string) => {
+  if (string.trim().length === 0) {
+    return "invalid input";
+  }
+
+  word = string.split(" ");
+  const result = word.reduce(
+    (acc, crt) => (crt.length > acc.length ? crt : acc),
+    " "
+  );
+  return result;
+};
+
+const result2 = longestString2(text);
+
+console.log("using reduce :", result2);
--- SmallestNumber.js
+++
@@ -1,1 +0,0 @@
-console.log("Problem Longest Word");
