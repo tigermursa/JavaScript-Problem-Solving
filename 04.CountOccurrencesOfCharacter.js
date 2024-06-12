a7ceb7cd9ab90c1d39a6ff83566dbd24649defe2
tigermursa <mursalin2311@gmail.com>
2024-06-12 23:07:41 +0600

    problem 4 done

---
+++ 04.CountOccurrencesOfCharacter.js
@@ -0,0 +1,21 @@
+//!=======================================================
+//!       Problem : Count Occurrences Of Character
+//!=======================================================
+
+//Expected console.log(countChar("wow","w")); // output : 2
+
+const countChar = (word, char) => {
+  word = word.toLowerCase();
+  char = char.toLowerCase();
+
+  const totalCount = word.split("").reduce((acc, crt) => {
+    if (crt === char) {
+      acc++;
+    }
+    return acc;
+  }, 0);
+
+  return totalCount;
+};
+
+console.log(countChar("Bangladesh", "a"));
